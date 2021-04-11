57013bf5db824e1bcea8d276305941e5f381ad95
Faisal-Alqabbani <faisal11224455@gmail.com>
2021-04-11 16:31:35 +0300

    function basic 1

---
+++ python_fundamentals/function_basic_1/alqabbani_faisal.py
@@ -0,0 +1,123 @@
+#1
+def a():
+    return 5
+print(a()) # output: 5
+
+#2
+def a():
+    return 5
+print(a()+a()) # output: 10
+
+#3
+def a():
+    return 5
+    return 10
+print(a()) #output: 5
+
+
+#4
+def a():
+    return 5
+    print(10)
+print(a()) #output: 5
+
+#5
+def a():
+    print(5)
+x = a()
+print(x) # None because the function dose not return any value.
+
+#6
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3)) # output: error function dose not return any value.
+
+#7
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5)) #output: 25 
+
+#8
+def a():
+    b = 100 
+    print(b) # output: 100
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a()) # output: 100 10
+
+#9
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3)) # output: 7
+print(a(5,3)) # output: 14
+print(a(2,3) + a(5,3)) # output: 7 + 14 21
+
+10
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5)) # output: 8
+
+#11
+b = 500
+print(b) # output: 500
+def a():
+    b = 300
+    print(b)
+print(b) # output: 500
+a() # output: 300
+print(b) #output: 500
+
+
+#12
+b = 500
+print(b) #ouput: 500
+def a():
+    b = 300
+    print(b)
+    return b
+print(b) #output: 500
+a() #output: 300
+print(b) #output: 500 
+
+#13
+b = 500
+print(b) #ouput: 500
+def a():
+    b = 300
+    print(b)
+    return b 
+print(b) #output: 500
+b=a() #output: 300
+print(b) #output: 300
+
+
+#14
+def a():
+    print(1) #output: 1
+    b() # output: 3
+    print(2) #output: 2
+def b():
+    print(3)
+a() #output: 1,3,2
+
+#15
+def a():
+    print(1) 
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y) # output: 1,3,5,10
+
+
