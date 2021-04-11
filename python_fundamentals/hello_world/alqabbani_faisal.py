1df2a1f91ff29192d27270fefe875e3a5e9740f7
Faisal-Alqabbani <faisal11224455@gmail.com>
2021-04-11 14:36:28 +0300

    for loop basic 1

--- python_fundamentals/hello_world/alqabbani_faisal.py
+++ python_fundamentals/hello_world/alqabbani_faisal.py
@@ -9,7 +9,7 @@ print("Hello " + name) #with +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
 print("Hello", name) #with comma
-# print("Hello " + name) # with a +	-- this one should give us an error!
+print("Hello " + str(name)) # with a +	-- this one should give us an error!
 
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
